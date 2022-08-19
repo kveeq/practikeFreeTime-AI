e14c83f6b7b34d3f1b18c060d3c70360a74ffc98
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2022-08-19 13:17:31 +0300

    ff

--- Yields/Program.cs
+++ Yields/Program.cs
@@ -47,39 +47,6 @@ namespace Yields
 
     public class Program
     {
-        private T data;
-        private LinkedList<NTree<T>> children;
-
-        public NTree(T data)
-        {
-            this.data = data;
-            children = new LinkedList<NTree<T>>();
-        }
-
-        public void AddChild(T data)
-        {
-            children.AddFirst(new NTree<T>(data));
-        }
-
-        public NTree<T> GetChild(int i)
-        {
-            foreach (NTree<T> n in children)
-                if (--i == 0)
-                    return n;
-            return null;
-        }
-
-        public void Traverse(NTree<T> node, TreeVisitor<T> visitor)
-        {
-            visitor(node.data);
-            foreach (NTree<T> kid in node.children)
-                Traverse(kid, visitor);
-            Console.WriteLine();
-        }
-    }
-
-    public class Program
-    {
         public static void Main()
         {
             //MachineLearning.Class1.QuestionEvent = () => Console.ReadLine(); // передавать метод для возврата ответа к вопросу ассистента (return текст из распознанного текста)
